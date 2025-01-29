f55d22f02540dd5781de942484ed1bf3f6d94675
Alika33 <alika100605@gmail.com>
2025-01-29 10:43:44 +0200

    Add files via upload

--- genius-test/js/mobile-menu.js
+++ genius-test/js/mobile-menu.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const mobileMenu = document.querySelector(".mobile-menu");
   const menuBtnOpen = document.querySelector(".menu-btn-open");
   const menuBtnClose = document.querySelector(".menu-btn-close");
-  const navLinks = document.querySelectorAll(".header__nav .header__link");
+  const navMenu = document.querySelector(".mobile-menu__nav");
 
   const toggleMenu = () => {
     mobileMenu.classList.toggle("is-open");
@@ -11,24 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
   menuBtnOpen.addEventListener("click", toggleMenu);
   menuBtnClose.addEventListener("click", toggleMenu);
 
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
+  document.querySelectorAll(".mobile-menu__link").forEach((n) =>
+    n.addEventListener("click", () => {
       mobileMenu.classList.remove("is-open");
-    });
-  });
-  // Закрыть меню при клике вне его
-  document.getElementById("menu").addEventListener("click", (event) => {
-    event._isClickWithInMenu = true;
-  });
-
-  document.getElementById("burger").addEventListener("click", (event) => {
-    event._isClickWithInMenu = true;
-  });
-
-  document.body.addEventListener("click", (event) => {
-    if (event._isClickWithInMenu) return;
-
-    // Действие при клике
-    document.querySelector(".header").classList.remove("open");
-  });
+      navMenu.classList.remove("is-open");
+    })
+  );
 });
